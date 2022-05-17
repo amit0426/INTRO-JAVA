a65ca28bdbf9ea470a77beaeae66c1297195b1c0
mathura123 <101566236+mathura123@users.noreply.github.com>
2022-05-17 05:41:10 +0530

    Add files via upload
    introduction of java assisment

---
+++ Amit.java
@@ -0,0 +1,107 @@
+
+Q5) Write a Java Application which prints your details?
+
+
+
+public class Amit {
+    
+ public static void main(String[] args) {
+        
+     
+
+        String name = "amitkumar";
+        String fathername = "Vijay Singh";
+        String mothername = "Soorajmukhi";
+        int age = 23;
+        String gender = "male";
+        String address = "229 plot Govind vihar mathura";
+        String mobi = "7830137966";
+   System.out.println("Name:"+" "+name);
+   System.out.println("Father name:"+" " +fathername);
+   System.out.println("mothername:"+" "+mothername);
+   System.out.println("Age:"+" "+age);
+   System.out.println("Gender:" +" "+gender);
+   System.out.println("address:"+" "+address);
+   System.out.println("mobile:"+" "+mobi);
+        
+    }
+}
+
+//Name: amitkumar
+Father name: Vijay Singh
+mothername: Soorajmukhi
+Age: 23
+Gender: male
+address: 229 plot Govind vihar mathura
+mobile: 7830137966//
+
+
+
+
+Q1) Write the difference between JRE, JDK and JVM?
+
+JRE MEAN - JAVA RUNTIME ENVIRONMENT--(JVM + PREDEFINED LIBRARY CLASSES)
+JDK MEAN - JAVA DEVELOPMENT KIT----(JRE + DEVELOPMENT TOOLS LIKE JAVA COMPILER, DEBUGGER,ETC)
+JVM MEAN - JAVA VIRTUAL MACHINE
+
+JRE - it is use of implementation in jvm designed to execute java programs.
+provide programming tools and deployment technologies.
+JDK - it is use to develop java applications.
+JVM - it convert bytcode to machine specific code it is platform dependent 
+necessary in both jdk and jre.
+
+
+
+
+Q2)Write the difference between JSE, JEE and JME?
+JSE- JAVA STANDARD EDITION -> You can develop standlone application/desktop.
+applications or even write scripts using selenium tool for automation testing.
+
+
+JEE - JAVA ENTERPRISE EDITION -> it's used to develop rich enterprise web applicattions.
+
+JME - JAVA MICRO EDITION -> can be used to develop applications runing on mobile devices embedded systems etc.
+
+
+
+Q3) How does java achieve platform independency?
+java is platform independent because it does not depend on any type of plaform 
+javva is platform - independent language . in java programs are compiled into bytecode code and that byte code is 
+platform independent.
+
+
+
+
+Q4) What are features of Java and explain them?
+
+SIMPLE- object-oriented, simple syntax , write maintable code;
+
+PORTABLE -  platform independent complite into bytecode and bytecode convert into machine specific language;
+
+Secured ,ROBUST,ARCHITECTURE NEUTRAL , INTERPRETED , HIGH PERFORMANCE, MULTITHREADED,
+DISTRIBUTED, DYNAMIC;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
